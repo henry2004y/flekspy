64d927f1a8287843d2232e10a96bb08e8e3a3f3f
Hongyang Zhou <hyzhou@umich.edu>
2024-12-12 08:58:52 -0500

    Fix data downloading

--- tests/test_fleks.py
+++ tests/test_fleks.py
@@ -8,12 +8,14 @@ from flekspy.util.utilities import download_testfile
 
 filedir = os.path.dirname(__file__)
 
-if not os.path.isfile(filedir + "/data/bulk.1d.vlsv"):
+if not os.path.isfile(filedir + "/data/3d_raw.out"):
     url = "https://raw.githubusercontent.com/henry2004y/batsrus_data/master/batsrus_data.tar.gz"
-    download_testfile(url, "data")
-elif not os.path.isdir(filedir + "/data/test_particles"):
+    download_testfile(url, "tests/data")
+
+if not os.path.isdir(filedir + "/data/test_particles"):
     url = "https://raw.githubusercontent.com/henry2004y/batsrus_data/master/test_particles.tar.gz"
-    download_testfile(url, "data")
+    download_testfile(url, "tests/data")
+
 
 class TestIDL:
     files = ("1d__raw_2_t25.60000_n00000258.out",)
@@ -30,28 +32,31 @@ class TestIDL:
         with pytest.raises(FileNotFoundError):
             ds = flekspy.load("None")
 
+
 class TestAMReX:
-    files = ("z=0_fluid_region0_0_t00001640_n00010142.out", )
+    files = ("z=0_fluid_region0_0_t00001640_n00010142.out",)
     files = [os.path.join("tests/data/", file) for file in files]
 
     def test_load(self):
         ds = flekspy.load(self.files[0])
-        assert ds.data["uxS0"][2,1] == np.float32(-131.71918)
+        assert ds.data["uxS0"][2, 1] == np.float32(-131.71918)
         assert ds.data["uxS1"].shape == (601, 2)
 
+
 class TestParticles:
-    dirs = ("tests/data/test_particles", )
+    dirs = ("tests/data/test_particles",)
     from flekspy import FLEKSTP
 
     tp = FLEKSTP(dirs, iSpecies=1)
     pIDs = tp.getIDs()
     assert pIDs[0] == (0, 5121)
     traj = tp.read_particle_trajectory(pIDs[10])
-    assert traj[0,1] == -0.031386006623506546
+    assert traj[0, 1] == -0.031386006623506546
     x = tp.read_initial_location(pIDs[10])
-    assert x[1] == traj[0,1]
+    assert x[1] == traj[0, 1]
     ids, pData = tp.read_particles_at_time(0.0, doSave=False)
     assert ids[1][1] == 5129
+
 
 def load(files):
     """
@@ -61,9 +66,13 @@ def load(files):
     ds = flekspy.load(files[1])
     return ds
 
+
 def test_load(benchmark):
     path = "tests/data/"
-    files = ("1d__raw_2_t25.60000_n00000258.out", "z=0_fluid_region0_0_t00001640_n00010142.out")
+    files = (
+        "1d__raw_2_t25.60000_n00000258.out",
+        "z=0_fluid_region0_0_t00001640_n00010142.out",
+    )
     files = [os.path.join(path, file) for file in files]
 
     result = benchmark(load, files)
