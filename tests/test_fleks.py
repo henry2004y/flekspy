b38b62b13b365086f2df69aed32f9e6238733288
Hongyang Zhou <hyzhou@umich.edu>
2024-12-12 23:14:58 -0500

    Fix test path

--- tests/test_fleks.py
+++ tests/test_fleks.py
@@ -22,7 +22,7 @@ if not os.path.isdir(filedir + "/data/test_particles"):
 
 if not os.path.isdir(filedir + "/data/3d_particle_region0_1_t00000002_n00000007_amrex"):
     url = "https://raw.githubusercontent.com/henry2004y/batsrus_data/master/3d_particle.tar.gz"
-    download_testfile(url, "data")
+    download_testfile(url, "tests/data")
 
 
 class TestIDL:
