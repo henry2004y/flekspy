f371216b48dce2f9fedf56943e36b93b6e4b093c
Hongyang Zhou <hyzhou@umich.edu>
2025-07-03 10:13:47 -0400

    Refactor: faster and robust file selection

--- src/flekspy/__init__.py
+++ src/flekspy/__init__.py
@@ -4,30 +4,47 @@ flekspy Public API.
 
 from pathlib import Path
 import errno
+from itertools import islice
 from flekspy.idl import IDLData
 from flekspy.yt import FLEKSData, extract_phase
 from flekspy.tp import FLEKSTP
 
 
-def load(filename: str, iDomain=0, iSpecies=0, readFieldData: bool = False):
+def load(
+    filename: str,
+    iDomain: int = 0,
+    iSpecies: int = 0,
+    iFile: int = 0,
+    readFieldData: bool = False,
+):
     """Load FLEKS data.
 
     Args:
         filename (str): Input file name pattern.
         iDomain (int, optional): Test particle domain index. Defaults to 0.
         iSpecies (int, optional): Test particle species index. Defaults to 0.
+        iFile (int, optional): The index of the file to load if the pattern
+            matches multiple files. Defaults to 0.
         readFieldData (bool, optional): Whether or not to read field data for test particles. Defaults to False.
 
     Returns:
         FLEKS data: IDLData, FLEKSData, or FLEKSTP
     """
     p = Path(filename)
-    files = list(p.parent.glob(p.name))
+    file_generator = p.parent.rglob(p.name)
+    # Advance the generator to the iFile-th position and get the file.
+    selected_file_iter = islice(file_generator, iFile, iFile + 1)
+    try:
+        selected_file = next(selected_file_iter)
+    except StopIteration:
+        selected_file = None
 
-    if len(files) == 0:
+    if selected_file is None:
         message = f"No files found matching pattern: '{filename}'"
+        if iFile > 0:
+            message += f" at index {iFile}"
         raise FileNotFoundError(errno.ENOENT, message, filename)
-    filename = str(files[0].resolve())
+    filename = str(selected_file.resolve())
 
     filepath = Path(filename)
     basename = filepath.name
