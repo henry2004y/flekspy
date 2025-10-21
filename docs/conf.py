d82b9f560dcc93b28d6f1544f3b011427636932f
Hongyang Zhou <hyzhou@umich.edu>
2025-10-21 10:07:51 -0400

    Render math via mathjax (#104)
    * Try mathjax syntax without dollarmath
    
    * Put napoleon to top

--- docs/conf.py
+++ docs/conf.py
@@ -16,9 +16,9 @@ author = "Yuxi Chen, Hongyang Zhou"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.napoleon",
     "myst_nb",
     "autoapi.extension",
-    "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "sphinx.ext.mathjax",
 ]
